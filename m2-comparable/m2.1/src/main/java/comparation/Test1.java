1d37c9ebc0e24cce8f25595b0581e6f332d8f781
Zorgit <piligrimexe@Gmail.com>
2023-10-08 15:00:26 +0300

    Learn how to work with intergace Comparable

---
+++ m2-comparable/m2.1/src/main/java/comparation/Test1.java
@@ -0,0 +1,19 @@
+package comparation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Alex");
+        list.add("Иван");
+        list.add("Дмитрий");
+        System.out.println("Before sort");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println("After sort");
+        System.out.println(list);
+    }
+}
---
+++ m2-comparable/m2.1/src/main/java/comparation/Test2.java
@@ -0,0 +1,73 @@
+package comparation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args ) {
+        List<Employee> list = new ArrayList<>();
+        Employee emp1 = new Employee(100, "Zaur", "Tregulov", 12345);
+        Employee emp2= new Employee(15, "Ivan", "Petrov", 6542);
+        Employee emp3 = new Employee(123, "Ivan", "Sidorov", 8542);
+        list.add(emp1);
+        list.add(emp2);
+        list.add(emp3);
+        System.out.println("Before sorting \n" + list);
+        Collections.sort(list);
+        System.out.println("After sorting \n" + list);
+    }
+
+}
+
+class Employee implements Comparable<Employee> {
+    int id;
+    String name;
+    String surname;
+    int salary;
+
+    public Employee(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}' +"\n";
+    }
+
+    @Override
+    public int compareTo(Employee anotherEmp) {
+        //Больше - положительное, меньше - отрицательное, равны ноль
+    /* Способ 1
+        if (this.id == anotherEmp.id) {
+            return 0;
+        }
+        else if (this.id < anotherEmp.id) {
+            return -1;
+        } else {
+            return 1;
+        }
+
+     */
+        //return this.id - anotherEmp.id; //способ 2
+        // способ 3 return this.id.compareTo(anotherEmp.id) - для ссылочных
+        // типов String, Integer ....;
+        /* return this.name.compareTo(anotherEmp.name); //вариант для сортировки
+        // по имени в натуральном порядке. */
+        int res = this.name.compareTo(anotherEmp.name);
+        if (res == 0) {
+            res = this.surname.compareTo(anotherEmp.surname);
+        }
+        return res;
+    }
+
+}
