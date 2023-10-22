602f232560525b642f829437c66dd254e25bf46c
Zorgit <piligrimexe@Gmail.com>
2023-10-22 23:39:34 +0300

    ListIterator example

---
+++ m4-collection/src/main/java/collection/ListIteratorExample.java
@@ -0,0 +1,36 @@
+package collection;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListIteratorExample {
+    public static void main(String[] args) {
+        String s = "madam";
+        List<Character> list = new LinkedList<>();
+        for (char ch : s.toCharArray()) {
+            list.add(ch);
+        }
+        System.out.println(list);
+
+        ListIterator<Character> iterator = list.listIterator();
+        ListIterator<Character> reversIterator = list.listIterator(list.size());
+
+        boolean isPalindrome = true;
+
+        while (iterator.hasNext() && reversIterator.hasPrevious()) {
+            if (iterator.next() != reversIterator.previous()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("NOT Palindrome");
+        }
+
+
+    }
+
+}
