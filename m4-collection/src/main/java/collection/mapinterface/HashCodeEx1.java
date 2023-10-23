897ebbef23d80ea3e13298a2562ac16abccbf9c4
Zorgit <piligrimexe@Gmail.com>
2023-10-23 23:55:07 +0300

    Equals and HashCode examples

---
+++ m4-collection/src/main/java/collection/mapinterface/HashCodeEx1.java
@@ -0,0 +1,63 @@
+package collection.mapinterface;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HashCodeEx1 {
+    public static void main(String[] args) {
+        Map<Student, Double> map = new HashMap<>();
+        Student st1 = new Student("Zaur", "Tregulov", 3);
+        Student st2 = new Student("Mariya", "Ivanova", 1);
+        Student st3 = new Student("Sergey", "Petrov", 4);
+        map.put(st1, 7.5);
+        map.put(st2, 8.5);
+        map.put(st3, 9.5);
+        System.out.println(map);
+        Student st4 = new Student("Zaur", "Tregulov", 3);
+        Student st5 = new Student("Igor", "Sidorov", 4);
+//        boolean result = map.containsKey(st4);
+//        System.out.println("Result =" + result);
+        System.out.println(st1.hashCode());
+        System.out.println(st5.hashCode());
+        System.out.println(st4.hashCode());
+
+    }
+}
+
+
+class Student {
+    String name;
+    String surname;
+    int course;
+
+    public Student(String name, String surname, int course) {
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course && Objects.equals(name, student.name) && Objects.equals(surname, student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.length()*7 + surname.length()*11 + course*53;
+    }
+}
