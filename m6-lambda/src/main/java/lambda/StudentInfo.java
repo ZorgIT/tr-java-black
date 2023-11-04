ecc9efcadb83d820f03b116c4c2ac05b9709c370
Zorgit <piligrimexe@Gmail.com>
2023-11-05 00:24:59 +0300

    Function interface examples

--- m6-lambda/src/main/java/lambda/StudentInfo.java
+++ m6-lambda/src/main/java/lambda/StudentInfo.java
@@ -3,7 +3,9 @@ package lambda;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.function.Function;
 
 public class StudentInfo {
 
@@ -51,7 +53,7 @@ class Test {
         Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
         Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
         Student st4 = new Student("Petr", 'm', 35, 4, 7);
-        Student st5 = new Student("Mariya", 'f', 23, 3, 9.1);
+        Student st5 = new Student("Mariya", 'f', 23, 3, 7.4);
         ArrayList<Student> students = new ArrayList<>();
         students.add(st1);
         students.add(st2);
@@ -60,6 +62,13 @@ class Test {
         students.add(st5);
 
         StudentInfo info = new StudentInfo();
+
+
+
+
+//        //пример function
+//        Function<Student, Double> f = student -> student.avgGrade;
+
 //        Collections.sort(students, new Comparator<Student>() {
 //            @Override
 //            public int compare(Student s1, Student s2) {
@@ -122,15 +131,15 @@ class Test {
             return p.avgGrade > 8;
         });*/
 
-
-        //Объединение
-        Predicate<Student> p1 = student -> student.avgGrade > 7.5;
-        Predicate<Student> p2 = student -> student.sex == 'm';
-
-
-        //info.testStudents(students, p1.and(p2));
-        //info.testStudents(students, p1.negate())); //negate инвертирует фильтр
-        info.testStudents(students, p1.or(p2));
+//
+//        //Объединение
+//        Predicate<Student> p1 = student -> student.avgGrade > 7.5;
+//        Predicate<Student> p2 = student -> student.sex == 'm';
+//
+//
+//        //info.testStudents(students, p1.and(p2));
+//        //info.testStudents(students, p1.negate())); //negate инвертирует фильтр
+//        info.testStudents(students, p1.or(p2));
 
 /*
         System.out.println("----------------------");
@@ -146,7 +155,22 @@ class Test {
                 });
                 */
 
+        //function
+        Function<Student, Double> f = student -> student.avgGrade;
+        double res = avgOfSmth(students, student -> (double) student.age);
+        System.out.println(res);
 
+    }
+
+    private static double avgOfSmth(List<Student> list, Function<Student,
+            Double> f) {
+        double result = 0;
+        for (Student st : list
+        ) {
+            result += f.apply(st); // у ф инт Function есть метод apply
+        }
+        result = result / list.size();
+        return result;
 
     }
 }
